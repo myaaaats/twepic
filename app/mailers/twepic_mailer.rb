3e945646c6805427acaf654a88bb1f3b327e3f1e
Maya <maya_sato@diveintocode.jp>
2018-09-10 12:21:39 +0900

    アクションメーラーのメールの宛先を投稿者にする

--- app/mailers/twepic_mailer.rb
+++ app/mailers/twepic_mailer.rb
@@ -1,6 +1,6 @@
 class TwepicMailer < ApplicationMailer
   def twepic_mail(twepic)
     @twepic = twepic
-     mail to: "maya_sato@diveintocode.jp", subject: "お問い合わせの確認メール"
+     mail to: @twepic.user.email, subject: "お問い合わせの確認メール"
   end
 end
