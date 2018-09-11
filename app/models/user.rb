70b4b00ed2a001885aca5b4716239943c59e1dce
Maya <maya_sato@diveintocode.jp>
2018-09-11 12:37:05 +0900

    ユーザーの画像登録をできるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :twepics
   has_many :favorites, dependent: :destroy
   has_many :favorite_twepics, through: :favorites, source: :twepic
+  mount_uploader :image, ImageUploader
 end
