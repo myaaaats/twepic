69d2b3bac9d9e5dd43b4bc434bb1bed4dc71a9dc
Maya <maya_sato@diveintocode.jp>
2018-09-12 13:28:12 +0900

    投稿時のメールタイトルの変更

--- app/mailers/twepic_mailer.rb
+++ app/mailers/twepic_mailer.rb
@@ -1,6 +1,6 @@
 class TwepicMailer < ApplicationMailer
   def twepic_mail(twepic)
     @twepic = twepic
-     mail to: @twepic.user.email, subject: "お問い合わせの確認メール"
+     mail to: @twepic.user.email, subject: "Twepic投稿確認メール"
   end
 end
